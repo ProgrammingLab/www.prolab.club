f0af866d45937fcc80de1485bcb095827d88d7f6
Yama <yoshiaki.suyama@suyama.ne.jp>
2024-02-07 19:50:13 +0900

    [frontend] Fix runtime setting

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -7,6 +7,8 @@ import { Navbar } from "@/components/navbar";
 import { Link } from "@nextui-org/link";
 import clsx from "clsx";
 
+export const runtime = 'edge';
+
 export const metadata: Metadata = {
 	title: {
 		default: siteConfig.name,
