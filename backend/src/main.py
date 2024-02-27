a8f5f1c01629ad05a8cc27bbcf63b5d2b55d32dd
1206marakasu <161473092+1206marakasu@users.noreply.github.com>
2024-02-27 22:39:41 +0900

    Add backend

---
+++ backend/src/main.py
@@ -0,0 +1,19 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+with open("sample.txt", "r", encoding="utf-8") as file:
+    data = file.read()
+cnt = int(data)
+
+@app.route("/hello")
+def hello():
+    global cnt
+    cnt += 1
+    with open("sample.txt", "w+", encoding="utf-8") as file:
+        file.write(str(cnt))
+    return "アクセス数" + str(cnt)
+
+# 実行
+if __name__ == '__main__':
+    app.run()
